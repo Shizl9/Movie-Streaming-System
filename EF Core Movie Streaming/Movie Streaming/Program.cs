11beb72228c6837496093ec4fd37fc3f4428f31b
Sheika <sheika02x2@gmail.com>
2026-05-17 15:25:00 +0400

    adding initial comment

---
+++ EF Core Movie Streaming/Movie Streaming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie_Streaming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
